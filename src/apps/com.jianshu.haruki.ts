c8c926f17e0e964fe42b94642edb476939fc8d16
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-07-21 22:27:20 +0800

    perf: any rules

--- src/apps/com.jianshu.haruki.ts
+++ src/apps/com.jianshu.haruki.ts
@@ -36,14 +36,46 @@ export default defineGkdApp({
     },
     {
       key: 3,
-      name: '局部广告-首页信息流广告',
+      name: '局部广告-信息流广告',
       desc: '点击关闭',
       fastQuery: true,
       rules: [
         {
+          name: '快手、腾讯广告SDK-局部广告',
           matches: 'ImageView[vid="nativeAdClose"]',
-          activityIds: 'com.baiji.jianshu.MainActivity',
-          snapshotUrls: 'https://i.gkd.li/i/16201541',
+          snapshotUrls: [
+            'https://i.gkd.li/i/16201541',
+            'https://i.gkd.li/i/16324702',
+          ],
+        },
+      ],
+    },
+    {
+      key: 8,
+      name: '全屏广告-弹窗广告',
+      rules: [
+        {
+          name: '腾讯广告SDK-全屏弹窗',
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > FrameLayout[childCount=1] > ImageView[visibleToUser=true][width<80][height<80][vid=null]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/16324561',
+            'https://i.gkd.li/i/16324692',
+            'https://i.gkd.li/i/16324634',
+            'https://i.gkd.li/i/16324713', // 误触，add [visibleToUser=true]
+          ],
+        },
+      ],
+    },
+    {
+      key: 9,
+      name: '局部广告-文章页-底部悬浮卡片',
+      activityIds: 'com.jianshu.wireless.articleV2.X5ArticleDetailActivity',
+      rules: [
+        {
+          name: '字节广告SDK-底部悬浮卡片',
+          matches: 'ImageView[vid="mCloseView"]',
+          snapshotUrls: 'https://i.gkd.li/i/16324702',
         },
       ],
     },
